b88b1d8ad9362ab6e27b3ecb065ac38f30ec2221
Brett Shwom <brett.shwom@nyu.edu>
2014-02-26 13:34:00 -0500

    require jquery

--- index.js
+++ index.js
@@ -1,52 +1,79 @@
-/******************************************************************************/
-/*********************************** EASING ***********************************/
-/******************************************************************************/
+(function ( global, factory ) {
 
-(function() {
+  'use strict';
 
-// based on easing equations from Robert Penner (http://www.robertpenner.com/easing)
+  // Common JS (i.e. browserify) environment
+  if ( typeof module !== 'undefined' && module.exports && typeof require === 'function' ) {
+    factory( require( 'jquery' ) );
+  }
 
-var baseEasings = {};
+  // AMD?
+  else if ( typeof define === 'function' && define.amd ) {
+    define([ 'jquery' ], factory );
+  }
 
-$.each( [ "Quad", "Cubic", "Quart", "Quint", "Expo" ], function( i, name ) {
-	baseEasings[ name ] = function( p ) {
-		return Math.pow( p, i + 2 );
-	};
-});
+  // browser global
+  else if ( global.$ ) {
+    factory( global.$ );
+  }
 
-$.extend( baseEasings, {
-	Sine: function ( p ) {
-		return 1 - Math.cos( p * Math.PI / 2 );
-	},
-	Circ: function ( p ) {
-		return 1 - Math.sqrt( 1 - p * p );
-	},
-	Elastic: function( p ) {
-		return p === 0 || p === 1 ? p :
-			-Math.pow( 2, 8 * (p - 1) ) * Math.sin( ( (p - 1) * 80 - 7.5 ) * Math.PI / 15 );
-	},
-	Back: function( p ) {
-		return p * p * ( 3 * p - 2 );
-	},
-	Bounce: function ( p ) {
-		var pow2,
-			bounce = 4;
+  else {
+    throw new Error( 'Could not find jquery!' );
+  }
 
-		while ( p < ( ( pow2 = Math.pow( 2, --bounce ) ) - 1 ) / 11 ) {}
-		return 1 / Math.pow( 4, 3 - bounce ) - 7.5625 * Math.pow( ( pow2 * 3 - 2 ) / 22 - p, 2 );
-	}
-});
 
-$.each( baseEasings, function( name, easeIn ) {
-	$.easing[ "easeIn" + name ] = easeIn;
-	$.easing[ "easeOut" + name ] = function( p ) {
-		return 1 - easeIn( 1 - p );
-	};
-	$.easing[ "easeInOut" + name ] = function( p ) {
-		return p < 0.5 ?
-			easeIn( p * 2 ) / 2 :
-			1 - easeIn( p * -2 + 2 ) / 2;
-	};
-});
+}( typeof window !== 'undefined' ? window : this, function ( $ ) {
 
-})();
+
+  /******************************************************************************/
+  /*********************************** EASING ***********************************/
+  /******************************************************************************/
+
+
+
+  // based on easing equations from Robert Penner (http://www.robertpenner.com/easing)
+
+  var baseEasings = {};
+
+  $.each( [ "Quad", "Cubic", "Quart", "Quint", "Expo" ], function( i, name ) {
+  	baseEasings[ name ] = function( p ) {
+  		return Math.pow( p, i + 2 );
+  	};
+  });
+
+  $.extend( baseEasings, {
+  	Sine: function ( p ) {
+  		return 1 - Math.cos( p * Math.PI / 2 );
+  	},
+  	Circ: function ( p ) {
+  		return 1 - Math.sqrt( 1 - p * p );
+  	},
+  	Elastic: function( p ) {
+  		return p === 0 || p === 1 ? p :
+  			-Math.pow( 2, 8 * (p - 1) ) * Math.sin( ( (p - 1) * 80 - 7.5 ) * Math.PI / 15 );
+  	},
+  	Back: function( p ) {
+  		return p * p * ( 3 * p - 2 );
+  	},
+  	Bounce: function ( p ) {
+  		var pow2,
+  			bounce = 4;
+
+  		while ( p < ( ( pow2 = Math.pow( 2, --bounce ) ) - 1 ) / 11 ) {}
+  		return 1 / Math.pow( 4, 3 - bounce ) - 7.5625 * Math.pow( ( pow2 * 3 - 2 ) / 22 - p, 2 );
+  	}
+  });
+
+  $.each( baseEasings, function( name, easeIn ) {
+  	$.easing[ "easeIn" + name ] = easeIn;
+  	$.easing[ "easeOut" + name ] = function( p ) {
+  		return 1 - easeIn( 1 - p );
+  	};
+  	$.easing[ "easeInOut" + name ] = function( p ) {
+  		return p < 0.5 ?
+  			easeIn( p * 2 ) / 2 :
+  			1 - easeIn( p * -2 + 2 ) / 2;
+  	};
+  });
+
+}))
